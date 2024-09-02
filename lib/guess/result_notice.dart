b830efc98179c2a89b61a2afb8386a598b0d3a53
jhb <2644263665@qq.com>
2024-09-02 17:39:07 +0800

    feat: 实现了ResultNotice组件的动画效果
    Signed-off-by: jhb <2644263665@qq.com>

--- lib/guess/result_notice.dart
+++ lib/guess/result_notice.dart
@@ -1,22 +1,53 @@
 import 'package:flutter/material.dart';
 
-class ResultNotice extends StatelessWidget {
+class ResultNotice extends StatefulWidget{
   const ResultNotice({super.key, required this.color, required this.info});
 
   final Color color;
   final String info;
 
   @override
+  State<ResultNotice> createState() => _ResultNoticeState();
+}
+
+class _ResultNoticeState extends State<ResultNotice>  with TickerProviderStateMixin  {
+  late AnimationController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(vsync: this, duration: const Duration(milliseconds: 500));
+    _controller.forward(from: 0);
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+
+  @override
+  void didChangeDependencies() {
+    super.didChangeDependencies();
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Container(
       alignment: Alignment.center,
-      color: color,
-      child: Text(
-        info,
-        style:const TextStyle(
-            fontSize: 54,
-            color: Colors.white,
-            fontWeight: FontWeight.bold),
+      color: widget.color,
+      child: AnimatedBuilder(
+        animation: _controller,
+        builder: (context, child) {
+          return Text(
+            widget.info,
+            style: TextStyle(
+                fontSize: 54 * (_controller.value),
+                color: Colors.white,
+                fontWeight: FontWeight.bold),
+          );
+        }
       ),
     );
   }
