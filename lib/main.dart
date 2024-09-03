e5073616d575ceeea214ca87c6ab5e2154b9e02c
jhb <2644263665@qq.com>
2024-09-03 11:32:56 +0800

    feat: 实现了电子木鱼页面的基础布局与交互
    Signed-off-by: jhb <2644263665@qq.com>

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:fun_toolbox/guess/guess_page.dart';
+
+import 'muyu/muyu_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -16,7 +17,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const GuessPage(),
+      home: const MuyuPage(),
     );
   }
 }
