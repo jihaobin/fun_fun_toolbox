06ad601b560763c9e586e2920ada877f5cfe3cf6
jhb <2644263665@qq.com>
2024-09-04 18:31:41 +0800

    feat: 实现待办事项基本布局
    Signed-off-by: jhb <2644263665@qq.com>

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
-
-import 'muyu/muyu_page.dart';
+import 'package:fun_toolbox/todo/todo_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -17,7 +16,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MuyuPage(),
+      home: const TodoPage(),
     );
   }
 }
